5e034b15bf3743578b26dd13fcb8cb22bf04ddaf
PriyadiSentosa <priyadisentosa825@gmail.com>
2022-02-12 10:40:34 +0700

    penjualan buku

---
+++ resources/views/frontend/index.blade.php
@@ -0,0 +1,325 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Penjualan Buku</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="assets/img/favicon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+
+    <!-- =======================================================
+  * Template Name: Hidayah - v4.7.0
+  * Template URL: https://bootstrapmade.com/hidayah-free-simple-html-template-for-corporate/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+    <!-- ======= Header ======= -->
+    <header id="header" class="fixed-top header-inner-pages">
+        <div class="container d-flex align-items-center justify-content-between">
+
+            <h1 class="logo"><a href="index.html">Penjualan Buku</a></h1>
+            <!-- Uncomment below if you prefer to use an image logo -->
+            <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+            <nav id="navbar" class="navbar">
+                <ul>
+                    <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+                    <li><a class="nav-link  scrollto" href="#portfolio">product</a></li>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav><!-- .navbar -->
+
+        </div>
+    </header><!-- End Header -->
+
+    <!-- ======= Hero Section ======= -->
+    <section id="hero">
+        <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
+
+            <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
+
+            <div class="carousel-inner" role="listbox">
+
+                <!-- Slide 1 -->
+                <div class="carousel-item active" style="background-image: url(assets/img/slide/slide-1.jpg)">
+                    <div class="carousel-container">
+                        <div class="container">
+                            <h2 class="animated fadeInDown">Welcome to <span><br>Aplikasi Penjualan Buku </span></h2>
+
+                        </div>
+                    </div>
+                </div>
+
+                <a class="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
+                </a>
+                <a class="carousel-control-next" href="#heroCarousel" role="button" data-bs-slide="next">
+                    <span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
+                </a>
+
+            </div>
+    </section><!-- End Hero -->
+
+    <main id="main">
+
+
+        <!-- ======= Portfolio Section ======= -->
+        <section id="portfolio" class="portfolio">
+            <div class="container-fluid">
+
+                <div class="section-title">
+
+                    <h3>Check our <span>Toko Buku</span></h3>
+                    <p>Menjual Buku Buku Horor Pilihan Ternama Di Indonesia.</p>
+                </div>
+
+                <div class="row portfolio-container justify-content-center">
+
+                    <div class="col-xl-10">
+                        <div class="row">
+
+                            <div class="col-xl-3 col-lg-4 col-md-6 portfolio-item filter-app">
+                                <div class="portfolio-wrap">
+                                    <img src="assets/img/portfolio/KTJ.jpg" class="img-fluid" alt="">
+                                    <div class="portfolio-info">
+                                        <h4>App 1</h4>
+                                        <p>App</p>
+                                        <div class="portfolio-links">
+                                            <a href="assets/img/portfolio/KTJ.jpg" data-gallery="portfolioGallery"
+                                                class="portfolio-lightbox" title="App 1"><i
+                                                    class="bx bx-plus"></i></a>
+
+                                            <a href="frontend.portfolio-details.html" title="More Details"><i
+                                                    class="bx bx-link"></i></a>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div><!-- End portfolio item -->
+
+                            <div class="col-xl-3 col-lg-4 col-md-6 portfolio-item filter-app">
+                                <div class="portfolio-wrap">
+                                    <img src="assets/img/portfolio/KTJ.jpg" class="img-fluid" alt="">
+                                    <div class="portfolio-info">
+                                        <h4>App 1</h4>
+                                        <p>App</p>
+                                        <div class="portfolio-links">
+                                            <a href="assets/img/portfolio/KTJ.jpg" data-gallery="portfolioGallery"
+                                                class="portfolio-lightbox" title="App 1"><i
+                                                    class="bx bx-plus"></i></a>
+
+                                            <a href="frontend.portfolio-details.html" title="More Details"><i
+                                                    class="bx bx-link"></i></a>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div><!-- End portfolio item -->
+
+                            <div class="col-xl-3 col-lg-4 col-md-6 portfolio-item filter-app">
+                                <div class="portfolio-wrap">
+                                    <img src="assets/img/portfolio/KTJ.jpg" class="img-fluid" alt="">
+                                    <div class="portfolio-info">
+                                        <h4>App 1</h4>
+                                        <p>App</p>
+                                        <div class="portfolio-links">
+                                            <a href="assets/img/portfolio/KTJ.jpg" data-gallery="portfolioGallery"
+                                                class="portfolio-lightbox" title="App 1"><i
+                                                    class="bx bx-plus"></i></a>
+
+                                            <a href="frontend.portfolio-details.html" title="More Details"><i
+                                                    class="bx bx-link"></i></a>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div><!-- End portfolio item -->
+
+                            <div class="col-xl-3 col-lg-4 col-md-6 portfolio-item filter-app">
+                                <div class="portfolio-wrap">
+                                    <img src="assets/img/portfolio/KTJ.jpg" class="img-fluid" alt="">
+                                    <div class="portfolio-info">
+                                        <h4>App 1</h4>
+                                        <p>App</p>
+                                        <div class="portfolio-links">
+                                            <a href="assets/img/portfolio/KTJ.jpg" data-gallery="portfolioGallery"
+                                                class="portfolio-lightbox" title="App 1"><i
+                                                    class="bx bx-plus"></i></a>
+
+                                            <a href="frontend.portfolio-details.html" title="More Details"><i
+                                                    class="bx bx-link"></i></a>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div><!-- End portfolio item -->
+
+                            <div class="col-xl-3 col-lg-4 col-md-6 portfolio-item filter-app">
+                                <div class="portfolio-wrap">
+                                    <img src="assets/img/portfolio/KTJ.jpg" class="img-fluid" alt="">
+                                    <div class="portfolio-info">
+                                        <h4>App 1</h4>
+                                        <p>App</p>
+                                        <div class="portfolio-links">
+                                            <a href="assets/img/portfolio/KTJ.jpg" data-gallery="portfolioGallery"
+                                                class="portfolio-lightbox" title="App 1"><i
+                                                    class="bx bx-plus"></i></a>
+
+                                            <a href="frontend.portfolio-details.html" title="More Details"><i
+                                                    class="bx bx-link"></i></a>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div><!-- End portfolio item -->
+
+                            <div class="col-xl-3 col-lg-4 col-md-6 portfolio-item filter-app">
+                                <div class="portfolio-wrap">
+                                    <img src="assets/img/portfolio/KTJ.jpg" class="img-fluid" alt="">
+                                    <div class="portfolio-info">
+                                        <h4>App 1</h4>
+                                        <p>App</p>
+                                        <div class="portfolio-links">
+                                            <a href="assets/img/portfolio/KTJ.jpg" data-gallery="portfolioGallery"
+                                                class="portfolio-lightbox" title="App 1"><i
+                                                    class="bx bx-plus"></i></a>
+
+                                            <a href="frontend.portfolio-details.html" title="More Details"><i
+                                                    class="bx bx-link"></i></a>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div><!-- End portfolio item -->
+
+                            <div class="col-xl-3 col-lg-4 col-md-6 portfolio-item filter-app">
+                                <div class="portfolio-wrap">
+                                    <img src="assets/img/portfolio/KTJ.jpg" class="img-fluid" alt="">
+                                    <div class="portfolio-info">
+                                        <h4>App 1</h4>
+                                        <p>App</p>
+                                        <div class="portfolio-links">
+                                            <a href="assets/img/portfolio/KTJ.jpg" data-gallery="portfolioGallery"
+                                                class="portfolio-lightbox" title="App 1"><i
+                                                    class="bx bx-plus"></i></a>
+
+                                            <a href="frontend.portfolio-details.html" title="More Details"><i
+                                                    class="bx bx-link"></i></a>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div><!-- End portfolio item -->
+
+                            <div class="col-xl-3 col-lg-4 col-md-6 portfolio-item filter-app">
+                                <div class="portfolio-wrap">
+                                    <img src="assets/img/portfolio/KTJ.jpg" class="img-fluid" alt="">
+                                    <div class="portfolio-info">
+                                        <h4>App 1</h4>
+                                        <p>App</p>
+                                        <div class="portfolio-links">
+                                            <a href="assets/img/portfolio/KTJ.jpg" data-gallery="portfolioGallery"
+                                                class="portfolio-lightbox" title="App 1"><i
+                                                    class="bx bx-plus"></i></a>
+
+                                            <a href="frontend.portfolio-details.html" title="More Details"><i
+                                                    class="bx bx-link"></i></a>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div><!-- End portfolio item -->
+
+                            <div class="col-xl-3 col-lg-4 col-md-6 portfolio-item filter-app">
+                                <div class="portfolio-wrap">
+                                    <img src="assets/img/portfolio/KTJ.jpg" class="img-fluid" alt="">
+                                    <div class="portfolio-info">
+                                        <h4>App 1</h4>
+                                        <p>App</p>
+                                        <div class="portfolio-links">
+                                            <a href="assets/img/portfolio/KTJ.jpg" data-gallery="portfolioGallery"
+                                                class="portfolio-lightbox" title="App 1"><i
+                                                    class="bx bx-plus"></i></a>
+
+                                            <a href="frontend.portfolio-details.html" title="More Details"><i
+                                                    class="bx bx-link"></i></a>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div><!-- End portfolio item -->
+
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </section><!-- End Portfolio Section -->
+
+    </main><!-- End #main -->
+
+    <!-- ======= Footer ======= -->
+    <footer id="footer">
+
+
+        <div class="container">
+            <div class="copyright">
+                &copy; Copyright <strong><span>Penjualan Buku</span></strong>. All Rights Reserved
+            </div>
+            <div class="credits">
+                <!-- All the links in the footer should remain intact. -->
+                <!-- You can delete the links only if you purchased the pro version. -->
+                <!-- Licensing information: https://bootstrapmade.com/license/ -->
+                <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/hidayah-free-simple-html-template-for-corporate/ -->
+                Designed by AF</a>
+            </div>
+        </div>
+    </footer><!-- End Footer -->
+
+    <div id="preloader"></div>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/purecounter/purecounter.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
